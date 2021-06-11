35f606d8ac3827d5808769b7e3f3f4aea33261b0
= <=>
2021-06-11 19:04:35 +0700

    Collections

---
+++ 14Collections/ListExample/src/main/java/vn/techmaster/Main.java
@@ -0,0 +1,11 @@
+package vn.techmaster;
+
+public class Main {
+    public static void main(String[] args) {
+//        ArrayListExample arrayList = new ArrayListExample();
+//        arrayList.methodInArrayList();
+
+        LinkedListExample linkedList = new LinkedListExample();
+        linkedList.methodInLinkedList();
+    }
+}
