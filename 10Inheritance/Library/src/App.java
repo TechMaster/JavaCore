2cd7b2fb6c9a252b00dea50888e6f4b12df803e3
= <=>
2021-07-07 20:30:05 +0700

    Library

---
+++ 10Inheritance/Library/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ShoolBook shoolBook = new ShoolBook();
+        shoolBook.input();
+
+        System.out.println(shoolBook.toString());
+    }
+}
