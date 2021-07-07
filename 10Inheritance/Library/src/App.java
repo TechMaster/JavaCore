17ffe23a48e66985bd13b9cafafa3517a414eddd
= <=>
2021-07-07 20:34:30 +0700

    update library

--- 10Inheritance/Library/src/App.java
+++ 10Inheritance/Library/src/App.java
@@ -1,8 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        ShoolBook shoolBook = new ShoolBook();
-        shoolBook.input();
 
-        System.out.println(shoolBook.toString());
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so luong sach can them: ");
+        int num = sc.nextInt();
+
+        ArrayList<ShoolBook> listBooks = new ArrayList<>();
+        for(int i = 0; i < num; i++){
+            ShoolBook shoolBook = new ShoolBook();
+            shoolBook.input();
+
+            listBooks.add(shoolBook);
+    
+        }
+        
+        System.out.println("Danh sach : ");
+        for (ShoolBook shoolBook : listBooks) {
+            System.out.println(shoolBook);
+        }
     }
 }
